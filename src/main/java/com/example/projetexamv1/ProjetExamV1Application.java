6a4af2d811569fb6672dd80064c3fd6c9e87c97e
raniaoul <oulhajrania1@gmail.com>
2024-01-11 14:51:56 +0100

    SecondCommit

--- src/main/java/com/example/projetexamv1/ProjetExamV1Application.java
+++ src/main/java/com/example/projetexamv1/ProjetExamV1Application.java
@@ -1,13 +1,35 @@
 package com.example.projetexamv1;
 
+import com.example.projetexamv1.Entity.CentreLocal;
+import com.example.projetexamv1.Entity.Habitant;
+import com.example.projetexamv1.Repository.CentreLocalRepository;
+import com.example.projetexamv1.Repository.HabitantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjetExamV1Application {
+
+    @Autowired
+    CentreLocalRepository centreLocalRepository;
+    @Autowired
+    HabitantRepository habitantRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetExamV1Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+                Habitant habitant = new Habitant("oul", "rania", "ibn sina", "agdal", "rabat", "oulhaj@gmail.com", "  0623456789");
+                CentreLocal centreLocal = new CentreLocal(22, "centreAgdal", "ibn sina", "agdal", "rabat");
+                habitant.setCentrePostalHabitant(centreLocal);
+                centreLocalRepository.save(centreLocal);
+                habitantRepository.save(habitant);
+            };
+        }
 }
--- src/test/java/com/example/projetexamv1/ProjetExamV1ApplicationTests.java
+++ src/test/java/com/example/projetexamv1/ProjetExamV1ApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.projetexamv1;
 
+import com.example.projetexamv1.Entity.CentreLocal;
+import com.example.projetexamv1.Entity.CentreRegional;
+import com.example.projetexamv1.Entity.Habitant;
+import com.example.projetexamv1.Repository.CentreLocalRepository;
+import com.example.projetexamv1.Repository.HabitantRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjetExamV1ApplicationTests {
 
+    @Autowired
+    HabitantRepository habitantRepository;
+    @Autowired
+    CentreLocalRepository centreLocalRepository;
+
+
     @Test
-    void contextLoads() {
+    public void SaveHabitant() {
+        Habitant habitant = new Habitant("oul", "rania","ibn sina", "agdal","rabat", "oulhaj@gmail.com", "  0623456789");
+        CentreLocal centreLocal = new CentreLocal(22, "centreAgdal", "ibn sina", "agdal", "rabat");
+        habitant.setCentrePostalHabitant(centreLocal);
+        centreLocalRepository.save(centreLocal);
+        habitantRepository.save(habitant);
     }
 
 }
